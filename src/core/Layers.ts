34b6ef8fa2b1a4d0a38d7773f9316f6c015c7624
getfile <getfilegetfile@gmail.com>
2017-12-31 22:51:18 +0800

    ...

--- src/core/Layers.ts
+++ src/core/Layers.ts
@@ -2,45 +2,42 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-function Layers() {
+class Layers
+{
 
-	this.mask = 1 | 0;
+	mask: number;
 
-}
-
-Object.assign( Layers.prototype, {
-
-	set: function ( channel ) {
-
-		this.mask = 1 << channel | 0;
-
-	},
-
-	enable: function ( channel ) {
-
-		this.mask |= 1 << channel | 0;
-
-	},
-
-	toggle: function ( channel ) {
-
-		this.mask ^= 1 << channel | 0;
-
-	},
-
-	disable: function ( channel ) {
-
-		this.mask &= ~ ( 1 << channel | 0 );
-
-	},
-
-	test: function ( layers ) {
-
-		return ( this.mask & layers.mask ) !== 0;
-
+	constructor()
+	{
+		this.mask = 1 | 0;
 	}
 
-} );
+	set( channel )
+	{
+		this.mask = 1 << channel | 0;
+	}
+
+	enable( channel )
+	{
+		this.mask |= 1 << channel | 0;
+	}
+
+	toggle( channel )
+	{
+		this.mask ^= 1 << channel | 0;
+	}
+
+	disable( channel )
+	{
+		this.mask &= ~( 1 << channel | 0 );
+	}
+
+	test( layers: Layers )
+	{
+		return ( this.mask & layers.mask ) !== 0;
+	}
+
+}
 
 
 export { Layers };
